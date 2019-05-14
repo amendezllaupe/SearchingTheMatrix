c58feff429f1148eeb87593778fdf904cba39c41
MarcosNavarrete <43323936+MarcosNavarrete@users.noreply.github.com>
2019-05-14 01:43:46 -0400

    Add files via upload

---
+++ SearchToMatrix22/src/searchtomatrix/Busqueda.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searchtomatrix;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Esteban
+ */
+public class Busqueda {
+
+    private ArrayList<Numero> registro;
+    
+    public Busqueda(){
+        registro = new ArrayList<>();
+        System.out.println("Ingrese el número de filas, columnas, y los números a guardar");
+        int[][] matriz = generarMatriz(ingresoInt(),ingresoInt());
+        System.out.println("Cantidad de números a buscar");
+        int nBuscar = ingresoInt();
+        mostrarMatriz(matriz);
+        for (int i = 0; i < nBuscar; i++) {
+            System.out.println("Ingrese el número a buscar");
+            int num = validacion();
+            buscar(matriz,num);
+        }
+        mostrarRegistro();
+    }
+    
+    public int ingresoInt(){
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        try {
+            num = sc.nextInt();
+        } catch (Exception e) {
+            num = ingresoInt();
+        }
+        return num;
+    }
+    
+    public int[][] generarMatriz(int nFilas, int nColumnas){
+        int[][] matriz = new int[nFilas][nColumnas];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = 0; c < matriz[f].length; c++) {
+                matriz[f][c] = ingresoInt();
+            }
+        }
+        return matriz;
+    }
+    
+    public boolean buscar(int [][]matriz , int numero){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if(matriz[i][j] == numero){
+                    registro.add(new Numero(numero,true,i,j));
+                    return true;
+                }
+            }
+        }
+        registro.add(new Numero(numero,false,-1,-1));
+        return false;
+    }
+    
+    public void mostrarMatriz(int [][]matriz){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(" [ " + matriz[i][j] +" ] ");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public void mostrarRegistro() {
+        for (int i = 0; i < this.registro.size(); i++) {
+            //System.out.println("Numero" + (i + 1) + ":");
+            System.out.print("numero: " + this.registro.get(i).getNumero()+", ");
+            System.out.print("estado: " + this.registro.get(i).getEstado()+", ");
+            System.out.print("Fila: " + this.registro.get(i).getFila()+", ");
+            System.out.print("Columna: " + this.registro.get(i).getColumna()+", ");
+            
+            System.out.println();
+        }
+    }
+    public int validacion() {
+        int dato = 0;
+        boolean error;
+        do {
+            try {
+                error = false;
+                Scanner teclado = new Scanner(System.in);
+                dato = teclado.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Numero ingresado no es valido, intente nuevamente ");
+                error = true;
+
+            }
+        } while (error);
+        return dato;
+    }
+}
---
+++ SearchToMatrix22/src/searchtomatrix/Numero.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searchtomatrix;
+
+/**
+ *
+ * @author Esteban
+ */
+public class Numero {
+    private int numero;
+    private boolean estado;
+    private int fila;
+    private int columna;
+    
+    public Numero(int numero, boolean estado, int fila, int columna){
+        this.numero = numero;
+        this.estado = estado;
+        this.fila = fila;
+        this.columna = columna;
+    }
+    
+    public void setNumero(int numero){
+        this.numero= numero;
+    }
+    
+    public int getNumero(){
+        return this.numero;
+    }
+    
+    public void setEstado(boolean estado){
+        this.estado=estado;
+    }
+    
+    public boolean getEstado(){
+        return this.estado;
+    }
+    
+    public void setFila(int fila){
+        this.fila = fila;
+    }
+    
+    public int getFila(){
+        return this.fila;
+    }
+    
+    public void setColumna(int columna){
+        this.columna=columna;
+    }
+    
+    public int getColumna(){
+        return this.columna;
+    }
+}
